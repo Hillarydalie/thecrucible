66092401f4623b9493391e2a72c161f1bd16bcf3
hillarydalie <hidalie@gmail.com>
2019-09-23 17:27:19 +0300

    Edit manage.py file

---
+++ manage.py
@@ -0,0 +1,18 @@
+from flask_script import Manager,Server
+from flask_migrate import Migrate,MigrateCommand
+from app import create_app,db
+from app.models import Author,Blog, Blogcomment
+
+app = create_app()
+
+manager =  Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('runserver',Server(use_debugger=True))
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def add_shell_context():
+    return {"db":db,"Author":Author}
+
+if __name__=="__main__":
+    manager.run()
