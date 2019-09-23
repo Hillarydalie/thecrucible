3cba3b850b9536969a32782fcd5beac3c7fbedb2
hillarydalie <hidalie@gmail.com>
2019-09-23 23:33:10 +0300

    Prepare for heroku deployment

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config:
 
     QUOTE_URL = "http://quotes.stormconsultancy.co.uk/random.json"
@@ -5,3 +7,11 @@ class Config:
     SECRET_KEY='jklsjgkljskljgiorjiosnklfiowpnbriorbo'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://hillarydalie:password@localhost:5432/thecrucible'
+    
+class TestConfig():
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hillarydalie:password@localhost:5432/thecrucible_test'
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+configurations = { 'development':Config, 'test':TestConfig, "production":ProdConfig }
