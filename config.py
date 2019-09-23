d3f5f1f31889d6d13ee0d2256b50006af2f7cdbf
hillarydalie <hidalie@gmail.com>
2019-09-23 17:27:34 +0300

    Add config file to the repository

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+
+    QUOTE_URL = "http://quotes.stormconsultancy.co.uk/random.json"
+    DEBUG="True"
+    SECRET_KEY='jklsjgkljskljgiorjiosnklfiowpnbriorbo'
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://hillarydalie:password@localhost:5432/thecrucible'
