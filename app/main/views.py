db279b97aaf354dd1f62e331fb6ba527c59d7006
hillarydalie <hidalie@gmail.com>
2019-09-23 23:34:15 +0300

    Test routes before deployment

--- app/main/views.py
+++ app/main/views.py
@@ -2,14 +2,17 @@ from flask import render_template, url_for,request,redirect
 from flask_login import login_required,current_user
 from . import main
 from .. import db
-from ..models import Blog
+from ..models import Blog,Blogcomment
 from datetime import datetime
+from app.requests import getQuotes
 
-@main.route("/")
+@main.route("/home")
 @login_required
 def homepage():
+    quote = getQuotes()
+    another_quote = getQuotes()
     blogs = Blog.query.all()
-    return render_template(("index.html"), blogs=blogs)
+    return render_template(("index.html"), blogs=blogs, quote = quote,another_quote = another_quote)
 
 @main.route("/newblog")
 @login_required
@@ -33,5 +36,30 @@ def new_blog():
 @login_required
 def blog_details(blog_id):
     blogpost = Blog.query.filter_by(id=blog_id).first()
-    print(blogpost,'********')
+    blogcomment = Blogcomment.query.filter_by(id = blog_id).all()
     return render_template('details.html', blogpost = blogpost)
+
+@main.route("/comment/save/<blogcomment_id>", methods=["GET","POST"])
+@login_required
+def save_comments(blogcomment_id):
+    if request.method == "POST":
+        content = request.form.get("content")
+        new_comment = Blogcomment(content=content, author=current_user, post_id=blogcomment_id)
+        db.session.add(new_comment)
+        db.session.commit()
+        return redirect(url_for("main.blog_details", blog_id=blogcomment_id))
+
+@main.route("/comment/delete/<blogcomment_id>")
+@login_required
+def delete_comment(blogcomment_id):
+    if request.method == "POST":
+        blogcomment = Blogcomment.query.filter_by(id = blogcomment_id).first()
+        new_comment = Blogcomment(content=content, author=current_user, post_id=blogcomment_id)
+        db.session.delete(new_comment)
+        db.session.commit()
+        return render_template("main.blog_details", blog_id=blogcomment_id)
+
+@main.route("/profile", methods=["GET", "POST"])
+@login_required
+def load_profile():
+    return render_template("profile.html")
