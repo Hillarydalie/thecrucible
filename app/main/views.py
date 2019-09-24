53b6d2d8d1667e7b082dd39cf6ea7605ee6638da
hillarydalie <hidalie@gmail.com>
2019-09-24 16:04:47 +0300

    Deploy heroku

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for,request,redirect
 from flask_login import login_required,current_user
 from . import main
 from .. import db
-from ..models import Blog,Blogcomment
+from ..models import Author,Blog,Blogcomment
 from datetime import datetime
 from app.requests import getQuotes
 
