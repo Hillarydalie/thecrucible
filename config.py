6fe1109b7b7ef4bdeefea2ebce5c13592830bdf5
hillarydalie <hidalie@gmail.com>
2019-09-25 12:28:06 +0300

    Migrations

--- config.py
+++ config.py
@@ -7,7 +7,14 @@ class Config:
     SECRET_KEY='jklsjgkljskljgiorjiosnklfiowpnbriorbo'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://hillarydalie:password@localhost:5432/thecrucible'
-    
+
+ # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 class TestConfig():
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hillarydalie:password@localhost:5432/thecrucible_test'
 
